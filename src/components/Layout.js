4b649b7e32270df15ce40040f624a36a62903db1
Mathieu Santostefano <msantostefano@jolicode.com>
2018-10-21 18:52:43 +0200

    Remove useless stuff

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Blog - welcomattic" />
     <Navbar />
     <div>{children}</div>
   </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,23 +8,18 @@ const Navbar = () => (
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
+          <h1>Blog welcomattic</h1>
         </Link>
       </div>
       <div className="navbar-start">
         <Link className="navbar-item" to="/about">
           About
         </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
       </div>
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://github.com/welcomattic"
           target="_blank"
           rel="noopener noreferrer"
         >
