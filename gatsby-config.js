b86f32005c537c08ee791bb48b05140d8f7f1280
Mathieu Santostefano <msantostefano@jolicode.com>
2019-04-19 14:28:37 +0200

    Fix typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
         matomoUrl: 'https://welcomattic.matomo.cloud',
         siteUrl: 'https://blog.welcomattic.com'
       }
-    }
+    },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
