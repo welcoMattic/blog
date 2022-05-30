f5a28460a38ffbcd2989f8c3aeff33636ab30fb2
Mathieu Santostefano <msantostefano@protonmail.com>
2022-05-30 16:45:16 +0200

    Add JoliTypo

--- build.sh
+++ build.sh
@@ -4,14 +4,19 @@ export RCLONE_CONFIG_MYS3_SECRET_ACCESS_KEY=$CELLAR_ADDON_KEY_SECRET
 export RCLONE_CONFIG_MYS3_ENDPOINT=$CELLAR_ADDON_HOST
 export RCLONE_CONFIG_MYS3_TYPE="s3"
 
-# Download and unpack rclone and hugo
+# Download and unpack rclone, hugo and jolitypo
 wget https://downloads.rclone.org/v$DL_RCLONE_VERSION/rclone-v$DL_RCLONE_VERSION-linux-amd64.zip
 wget https://github.com/gohugoio/hugo/releases/download/v$HUGO_VERSION/hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
+wget https://github.com/jolicode/JoliTypo/releases/download/v1.3.0/jolitypo-8.0.phar
 unzip rclone-v$DL_RCLONE_VERSION-linux-amd64.zip
 tar xvf hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
 
 # Build Twitter cards images
 ./bin/tcardgen -o static/tcard -t og-picture-assets/og-picture-template.png -f og-picture-assets/fonts content/veille/*.md
+
+# Execute JoliTypo
+chmod +x ./jolitypo-8.0.phar
+php jolitypo-8.0.phar
 
 # Execute the site generation with Hugo
 chmod +x ./hugo
@@ -20,4 +25,4 @@ chmod +x ./hugo
 # Sync the site to Cellar
 ./rclone-v$DL_RCLONE_VERSION-linux-amd64/rclone sync ./public mys3:$MY_DOMAIN --progress --s3-acl=public-read
 
-clever stop
+clever --help
