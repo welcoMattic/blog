28e30bd57a8e8abb121f7eb85936094bf196e600
Mathieu Santostefano <msantostefano@proton.me>
2025-11-05 14:51:41 +0100

    remove jolitypo

--- pre_build.sh
+++ pre_build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Fix micro typography
-wget https://github.com/jolicode/JoliTypo/releases/download/v1.3.0/jolitypo-8.0.phar
-chmod +x ./jolitypo-8.0.phar
-php jolitypo-8.0.phar
-
 # Build og cards images
-go install github.com/Ladicle/tcardgen@latest
-./go/bin/tcardgen -o static/tcard -t og-picture-assets/og-picture-template.png -f og-picture-assets/fonts content/veille/*.md
+wget https://github.com/Ladicle/tcardgen/releases/download/v0.9.0/tcardgen_Linux_x86_64.tar.gz
+tar -xzf tcardgen_Linux_x86_64.tar.gz
+rm tcardgen_Linux_x86_64.tar.gz
+./tcardgen -o static/tcard -t og-picture-assets/og-picture-template.png -f og-picture-assets/fonts content/veille/*.md
+rm tcardgen
